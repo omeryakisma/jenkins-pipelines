4dac639d0ae415dbcf04b5c5eaf5b23da61049ed
Cloud User <centos@ip-172-31-5-96.eu-west-1.compute.internal>
2020-04-02 19:41:08 +0000

    added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,19 +1,33 @@
 node { 
+  // comment here
+  properties([
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+    pipelineTriggers([pollSCM('* * * * *')])
+  ])
   stage("Stage1"){ 
-    echo "hello" 
+    git 'https://github.com/farrukh90/cool_website.git'
   } 
-  stage("Stage2"){ 
-    echo "hello" 
-  } 
-  stage("Ask for Input"){ 
-    input 'Should I proceed?' 
-  } 
-  stage("Stage4"){ 
-    echo "hello" 
+  stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.acirrustech.com              sudo yum install httpd -y
+		"""
+  }
+
+  stage("Copy Artifacts"){ 
+    sh """
+    scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
+		ssh centos@jenkins_worker1.yakisma.com              sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_worker1.yakisma.com              sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_worker1.yakisma.com				      sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_worker1.yakisma.com				      sudo chmod 777 /var/www/html/*
+		ssh centos@jenkins_worker1.yakisma.com              sudo systemctl restart httpd 
+    """
    } 
-  stage("Stage5"){ 
-    echo "hello" 
-  } 
+  stage("Restart Web Server"){ 
+    sh """
+		ssh centos@jenkins_worker1.yakisma.com              sudo systemctl restart httpd 
+    """
+   }
   stage("Send Notifications to Slack"){ 
     slackSend color: '#BADA55', message: 'Hello, World!' 
   } 
