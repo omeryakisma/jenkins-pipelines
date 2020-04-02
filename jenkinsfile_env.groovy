4388aba2b841352ffec0e1bb4937647a26231460
omeryakisma <59777917+omeryakisma@users.noreply.github.com>
2020-04-02 17:41:08 -0400

    Update jenkinsfile_env.groovy

--- jenkinsfile_env.groovy
+++ jenkinsfile_env.groovy
@@ -6,13 +6,13 @@ node {
 		parameters([
       choice(
         choices: [
-			    'dev1.yakisma.com', 
-			    'qa1.yakisma.com', 
-		  	  'stage1.yakisma.com', 
-		  	  'prod1.yakisma.com'
+	  'dev1.yakisma.com', 
+	  'qa1.yakisma.com', 
+	  'stage1.yakisma.com', 
+	  'prod1.yakisma.com'
         ], 
-			description: 'Please choose an environment', 
-			name: 'ENV'
+	description: 'Please choose an environment', 
+	name: 'ENV'
     )
     ])
   ])
@@ -20,24 +20,24 @@ node {
     git 'https://github.com/farrukh90/cool_website.git'
   } 
   stage("Install Prerequisites"){
-		sh """
-		ssh centos@${ENV}              sudo yum install httpd -y
-		"""
+  sh """
+  ssh centos@${ENV}                 sudo yum install httpd -y
+  """
   }
   
   stage("Copy Artifacts"){ 
     sh """
     scp -r *  centos@${ENV}:/tmp
-		ssh centos@${ENV}              sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@${ENV}              sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@${ENV}				      sudo chown centos:centos /var/www/html/
-		ssh centos@${ENV}				      sudo chmod 777 /var/www/html/*
-		ssh centos@${ENV}              sudo systemctl restart httpd 
+    ssh centos@${ENV}              sudo cp -r /tmp/index.html /var/www/html/
+    ssh centos@${ENV}              sudo cp -r /tmp/style.css /var/www/html/
+    ssh centos@${ENV}              sudo chown centos:centos /var/www/html/
+    ssh centos@${ENV}              sudo chmod 777 /var/www/html/*
+    ssh centos@${ENV}              sudo systemctl restart httpd 
     """
    } 
   stage("Restart Web Server"){ 
     sh """
-		ssh centos@dev1.yakisma.com             sudo systemctl restart httpd 
+    ssh centos@dev1.yakisma.com    sudo systemctl restart httpd 
     """
    }
   stage("Send Notifications to Slack"){ 
