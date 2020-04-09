228ed69898f590a0341f5973cea498af25f9b244
yakishma <59777917+yakishma@users.noreply.github.com>
2020-04-08 23:01:58 -0400

    Update jenkinsfile_env.groovy

--- jenkinsfile_env.groovy
+++ jenkinsfile_env.groovy
@@ -3,44 +3,57 @@ node {
   properties([
     buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
     pipelineTriggers([pollSCM('* * * * *')]),
-    parameters([
+		parameters([
       choice(
         choices: [
-	  'dev1.yakisma.com', 
-	  'qa1.yakisma.com', 
-	  'stage1.yakisma.com', 
-	  'prod1.yakisma.com'
+			    'dev1.yakisma.com', 
+			    'qa1.yakisma.com', 
+		  	  'stage1.yakisma.com', 
+		  	  'prod1.yakisma.com'
         ], 
-	description: 'Please choose an environment', 
-	name: 'ENV'
-    )
+			  description: 'Please choose an environment', 
+			  name: 'ENV'
+      ) 
+      choice(
+        choices: [
+          'v0.1', 
+          'v0.2', 
+          'v0.3', 
+          'v0.4', 
+          'v0.5'
+        ],  
+        description: 'Which version should we deploy?',  
+        name: 'Version'
+      )
     ])
   ])
-  stage("Pull FarrukH Branch"){ 
-    checkout([$class: 'GitSCM', branches: [[name: '*/FarrukH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/cool_website.git']]])
-  }
+  stage("Stage1"){ 
+    git 'https://github.com/farrukh90/cool_website.git'
+  } 
   stage("Install Prerequisites"){
-  sh """
-  ssh centos@${ENV}                 sudo yum install httpd -y
-  """
+		sh """
+		ssh centos@${ENV}                sudo yum install httpd -y
+		"""
   }
-  
   stage("Copy Artifacts"){ 
     sh """
     scp -r *  centos@${ENV}:/tmp
-    ssh centos@${ENV}              sudo cp -r /tmp/index.html /var/www/html/
-    ssh centos@${ENV}              sudo cp -r /tmp/style.css /var/www/html/
-    ssh centos@${ENV}              sudo chown centos:centos /var/www/html/
-    ssh centos@${ENV}              sudo chmod 777 /var/www/html/*
-    ssh centos@${ENV}              sudo systemctl restart httpd 
+		ssh centos@${ENV}              sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENV}              sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENV}				      sudo chown centos:centos /var/www/html/
+		ssh centos@${ENV}				      sudo chmod 777 /var/www/html/*
+		ssh centos@${ENV}              sudo systemctl restart httpd 
     """
    } 
   stage("Restart Web Server"){ 
     sh """
-    ssh centos@${ENV}    sudo systemctl restart httpd 
+		ssh centos@${ENV}             sudo systemctl restart httpd 
     """
    }
   stage("Send Notifications to Slack"){ 
     slackSend color: '#BADA55', message: 'Hello, World!' 
-  }  
+  } 
+  stage("Send Email to Support"){ 
+    mail bcc: '', body: 'Running', cc: 'support@company.com', from: '', replyTo: '', subject: 'Test', to: 'yakisma.omer@gmail.com' 
+  } 
 } 
