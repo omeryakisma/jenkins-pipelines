caf893d9830a5477c395b3678f70d85257fb4116
yakishma <59777917+yakishma@users.noreply.github.com>
2020-04-10 10:33:14 -0400

    Update artemis_delivery.groovy

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -34,7 +34,7 @@ node {
             )
         ])
     ])
-	stage("Pull '${Version}'"){
+	stage("Pull ${Version}"){
 		timestamps {
 			ws {
 				checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]]) 
