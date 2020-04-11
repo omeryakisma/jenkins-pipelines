7b7bc24e6566d84bbe7f40a24fc19e41923572d2
yakishma <59777917+yakishma@users.noreply.github.com>
2020-04-11 08:54:23 -0400

    Update artemis_delivery.groovy

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -1,1 +1,75 @@
-
+node { 
+	properties([
+        parameters([
+            choice(
+                choices:[
+				    '0.1', 
+				    '0.2', 
+				    '0.3', 
+				    '0.4', 
+				    '0.5',
+				    '0.6',
+				    '0.7',
+				    '0.8',
+				    '0.9',
+				    '1.0',
+			    ], 
+		        description: 'Which version of the app should I deploy? ', 
+		        name: 'Version'
+            ), 
+	        choice(
+                choices: [
+                    'dev1.yakisma.com',  
+                    'qa1.yakisma.com',  
+                    'stage1.yakisma.com',  
+                    'prod1.yakisma.com'
+		        ], 
+	            description: 'Please provide an environment to build the application', 
+	            name: 'ENVIR'
+            )
+        ])
+    ])
+	stage("Pull ${Version} For ${ENVIR}"){
+		timestamps {
+			ws {
+				checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]]) 
+            }
+		}
+	}
+	stage("Get Credentials"){
+		timestamps {
+			ws{
+				sh '''
+					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis
+				'''
+			}
+		}
+	}
+	stage("Build Docker Image"){     // make sure docker is installed on the vm
+		timestamps {                 // echo 1 > /proc/sys/vm/drop_caches
+			ws {
+				sh '''   
+					docker build -t artemis:${Version} .
+				'''
+			}
+		}
+	}
+	stage("Tag Image"){
+			timestamps {
+				ws {
+					sh '''
+						docker tag artemis:${Version} 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					'''
+					}
+				}
+			}
+	stage("Push Image"){  // into DockerHub, ECR, Jenkins and Artifactory, etc images folders/repositoreis
+		timestamps {
+			ws {
+				sh '''
+					docker push 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+				'''
+			}
+		}
+	}
+}
