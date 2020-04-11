498fa68f5369a5047ec94f930916f4b716579cbc
yakishma <59777917+yakishma@users.noreply.github.com>
2020-04-11 13:21:00 -0400

    Update artemis_delivery.groovy

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -49,7 +49,7 @@ node {
 		timestamps {                 // echo 1 > /proc/sys/vm/drop_caches
 			ws {
 				sh '''   
-			   		ssh centos@$jenkins_master.yakisma.com docker build -t artemis:${Version} .
+			   		docker build -t artemis:${Version} .
 				'''
 			}
 		}
