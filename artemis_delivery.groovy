e769a069da292d1bc0157d01074bcd3505e8eaaf
yakishma <59777917+yakishma@users.noreply.github.com>
2020-04-10 22:03:45 -0400

    Update artemis_delivery.groovy

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -34,7 +34,7 @@ node {
             )
         ])
     ])
-	stage("Pull '${Version}'"){
+	stage("Pull ${Version}"){
 		timestamps {
 			ws {
 				checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]]) 
@@ -54,7 +54,7 @@ node {
 		timestamps {
 			ws {
 				sh '''
-					docker build -t artemis:'${Version}' .
+					docker build -t artemis:${Version} .
 				'''
 			}
 		}
@@ -63,7 +63,7 @@ node {
 			timestamps {
 				ws {
 					sh '''
-						docker tag artemis:'${Version}' 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:'${Version}'
+						docker tag artemis:${Version} 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
 					'''
 					}
 				}
@@ -72,7 +72,7 @@ node {
 		timestamps {
 			ws {
 				sh '''
-					docker push 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:'${Version}'
+					docker push 771745960392.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
 				'''
 			}
 		}
